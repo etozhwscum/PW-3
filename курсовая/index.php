c51c82e6a2adccf9c58b78dbd07b5340782a0467
etozhwscum <72293836+etozhwscum@users.noreply.github.com>
2021-04-17 15:07:15 +0700

    ну короче вот почти даааа

---
+++ курсовая/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Main</title>
+	<link rel="stylesheet" type="text/css" href="style/css.css">
+</head>
+<body>
+	<?php require('templates/header.php');?>
+	<?php require('templates/vacancy_main.php');?>
+	<?php require('templates/footer.php');?>
+</body>
+</html>
---
+++ курсовая/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/css.css">
+</head>
+<body>
+	<div class="header">
+		<header>
+		<a href="companions.php">Компаньоны</a>
+		<a href="vacancy.php">Вакансии</a>
+		<a href="index.php"><img src="images/logo.svg" width="75px"></a>
+		<a href="register.php">Аккаунт</a>
+		<a href="news.php">Новости</a>
+		</header>
+	</div>
+	<br><br>
+	<br><br>
+	<br><br>
+	<br><br>
+</body>
+</html>
